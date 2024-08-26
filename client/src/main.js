a15231165e4e8868a4923a2e563147c2a769e6e3
niy42 <obanlaniyi42@gmail.com>
2024-08-26 13:10:55 +0100

    modified globals.css

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import io from 'socket.io-client';
+import './globals.css';
 
 const app = createApp(App);
 
